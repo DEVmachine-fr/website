089b73903e37eb2a180c64553fc8d860e0d4fd0d
leroy-p <leroy-p@mgdis.fr>
2022-07-12 14:19:16 +0200

    feat: add accordion for savoir-faire page

---
+++ _plugins/collapsible.rb
@@ -0,0 +1,43 @@
+module Jekyll
+  module Tags
+    class CollapseTag < Liquid::Block
+      def initialize(tag_name, block_options, liquid_options)
+        super
+        @title = block_options.strip
+      end
+
+      def render(context)
+        accordionID = context["accordionID"]
+        idx = context["collapsed_idx"]
+        collapsedID = "#{accordionID}-collapse-#{idx}"
+        headingID = "#{accordionID}-heading-#{idx}"
+
+        # increment for the next collapsible
+        context["collapsed_idx"] = idx + 1
+
+        site = context.registers[:site]
+        content = super
+
+        output = <<~EOS
+          <div class="accordion-container">
+            <div id="#{headingID}">
+              <h2 class="mb-0">
+                <button class="accordion-btn collapsed" data-toggle="collapse" data-target="##{collapsedID}" aria-expanded="false" aria-controls="#{collapsedID}">
+                  <span class="collapse-title">#{@title}</span>
+                  <span aria-hidden="true" class="fas fa-chevron-up"></span>
+                </button>
+              </h2>
+            </div>
+            <div id="#{collapsedID}" class="accordion-content collapse" aria-labelledby="#{headingID}" data-parent="##{accordionID}">
+              #{content}
+            </div>
+          </div>
+        EOS
+
+        output
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('collapsible', Jekyll::Tags::CollapseTag)
